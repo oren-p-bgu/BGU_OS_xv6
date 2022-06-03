1dbafc3332ed4e9a24af3210517177ea8f924438
OrenP_BGU <80120095+oren-p-bgu@users.noreply.github.com>
2022-06-03 19:49:00 +0300

    Added helpful debug stuff - suspecting the problem is that cas wasn't added.

--- kernel/trap.c
+++ kernel/trap.c
@@ -17,16 +17,14 @@ void kernelvec();
 extern int devintr();
 
 void
-trapinit(void)
-{
-  initlock(&tickslock, "time");
+trapinit(void) {
+    initlock(&tickslock, "time");
 }
 
 // set up to take exceptions and traps while in the kernel.
 void
-trapinithart(void)
-{
-  w_stvec((uint64)kernelvec);
+trapinithart(void) {
+    w_stvec((uint64) kernelvec);
 }
 
 //
@@ -34,163 +32,159 @@ trapinithart(void)
 // called from trampoline.S
 //
 void
-usertrap(void)
-{
-  int which_dev = 0;
+usertrap(void) {
+    int which_dev = 0;
 
-  if((r_sstatus() & SSTATUS_SPP) != 0)
-    panic("usertrap: not from user mode");
+    if ((r_sstatus() & SSTATUS_SPP) != 0)
+        panic("usertrap: not from user mode");
 
-  // send interrupts and exceptions to kerneltrap(),
-  // since we're now in the kernel.
-  w_stvec((uint64)kernelvec);
+    // send interrupts and exceptions to kerneltrap(),
+    // since we're now in the kernel.
+    w_stvec((uint64) kernelvec);
 
-  struct proc *p = myproc();
-  
-  // save user program counter.
-  p->trapframe->epc = r_sepc();
+    struct proc *p = myproc();
 
-  if (r_scause() == 13 || r_scause() == 15){                                 // pagefault - Assignment 3
-      // pagefault - Assignment 3
-      uint64 start_va = PGROUNDDOWN(r_stval()); // Get virtual address of the start of the pagefault causing page
-      pte_t *pte;
-      pte = (pte_t *) walkaddr(p->pagetable, start_va);   // Get the page table entry of that page on the current process
-      if (pte == 0) {
-          printf("usertrap(): PROBLEM! Couldn't find page!\n");
-          p->killed = 1;
-      }
-      else if((*pte & PTE_V) && (*pte & PTE_U) && (*pte & PTE_COW)){
-          uint flags = PTE_FLAGS(*pte);
-          flags |= PTE_W;       // Add write permission to the page now.
-          flags &= (~PTE_COW);  // Will be copied, so no longer copy on write.
+    // save user program counter.
+    p->trapframe->epc = r_sepc();
 
-          char *mem = kalloc();
-          char *pa = (char *)PTE2PA(*pte);
-          memmove(mem, pa, PGSIZE);         // New copy of page
-          uvmunmap(p->pagetable, start_va, PGSIZE, 0);
+    if (r_scause() == 13 || r_scause() == 15) {                                 // pagefault - Assignment 3
+        // pagefault - Assignment 3
+        uint64 start_va = PGROUNDDOWN(r_stval()); // Get virtual address of the start of the pagefault causing page
+        pte_t *pte;
+        pte = (pte_t *) walkaddr(p->pagetable,
+                                 start_va);   // Get the page table entry of that page on the current process
+        if (pte == 0) {
+            printf("usertrap(): PROBLEM! Couldn't find page!\n");
+            p->killed = 1;
+        } else if ((*pte & PTE_V) && (*pte & PTE_U) && (*pte & PTE_COW)) {
+            uint flags = PTE_FLAGS(*pte);
+            flags |= PTE_W;       // Add write permission to the page now.
+            flags &= (~PTE_COW);  // Will be copied, so no longer copy on write.
 
-          if (mappages(p->pagetable, start_va, PGSIZE, (uint64)mem, flags) != 0) {
-              printf("usertrap(): PROBLEM! Something went wrong in mappages!\n");
-              p->killed = 1;
-          }
-      }
+            char *mem = kalloc();
+            char *pa = (char *) PTE2PA(*pte);
+            memmove(mem, pa, PGSIZE);         // New copy of page
+            uvmunmap(p->pagetable, start_va, PGSIZE, 0);
+            rem_ref((void*)pa);
+            if (mappages(p->pagetable, start_va, PGSIZE, (uint64) mem, flags) != 0) {
+                printf("usertrap(): PROBLEM! Something went wrong in mappages!\n");
+                p->killed = 1;
+            }
+        }
 
-  } else if(r_scause() == 8){
-    // system call
+    } else if (r_scause() == 8) {
+        // system call
 
-    if(p->killed)
-      exit(-1);
+        if (p->killed)
+            exit(-1);
 
-    // sepc points to the ecall instruction,
-    // but we want to return to the next instruction.
-    p->trapframe->epc += 4;
+        // sepc points to the ecall instruction,
+        // but we want to return to the next instruction.
+        p->trapframe->epc += 4;
 
-    // an interrupt will change sstatus &c registers,
-    // so don't enable until done with those registers.
-    intr_on();
+        // an interrupt will change sstatus &c registers,
+        // so don't enable until done with those registers.
+        intr_on();
 
-    syscall();
-  } else if((which_dev = devintr()) != 0){
-    // ok
-  } else {
-    printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
-    printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
-    p->killed = 1;
-  }
+        syscall();
+    } else if ((which_dev = devintr()) != 0) {
+        // ok
+    } else {
+        printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
+        printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
+        p->killed = 1;
+    }
 
-  if(p->killed)
-    exit(-1);
+    if (p->killed)
+        exit(-1);
 
-  // give up the CPU if this is a timer interrupt.
-  if(which_dev == 2)
-    yield();
+    // give up the CPU if this is a timer interrupt.
+    if (which_dev == 2)
+        yield();
 
-  usertrapret();
+    usertrapret();
 }
 
 //
 // return to user space
 //
 void
-usertrapret(void)
-{
-  struct proc *p = myproc();
+usertrapret(void) {
+    struct proc *p = myproc();
 
-  // we're about to switch the destination of traps from
-  // kerneltrap() to usertrap(), so turn off interrupts until
-  // we're back in user space, where usertrap() is correct.
-  intr_off();
+    // we're about to switch the destination of traps from
+    // kerneltrap() to usertrap(), so turn off interrupts until
+    // we're back in user space, where usertrap() is correct.
+    intr_off();
 
-  // send syscalls, interrupts, and exceptions to trampoline.S
-  w_stvec(TRAMPOLINE + (uservec - trampoline));
+    // send syscalls, interrupts, and exceptions to trampoline.S
+    w_stvec(TRAMPOLINE + (uservec - trampoline));
 
-  // set up trapframe values that uservec will need when
-  // the process next re-enters the kernel.
-  p->trapframe->kernel_satp = r_satp();         // kernel page table
-  p->trapframe->kernel_sp = p->kstack + PGSIZE; // process's kernel stack
-  p->trapframe->kernel_trap = (uint64)usertrap;
-  p->trapframe->kernel_hartid = r_tp();         // hartid for cpuid()
+    // set up trapframe values that uservec will need when
+    // the process next re-enters the kernel.
+    p->trapframe->kernel_satp = r_satp();         // kernel page table
+    p->trapframe->kernel_sp = p->kstack + PGSIZE; // process's kernel stack
+    p->trapframe->kernel_trap = (uint64) usertrap;
+    p->trapframe->kernel_hartid = r_tp();         // hartid for cpuid()
 
-  // set up the registers that trampoline.S's sret will use
-  // to get to user space.
-  
-  // set S Previous Privilege mode to User.
-  unsigned long x = r_sstatus();
-  x &= ~SSTATUS_SPP; // clear SPP to 0 for user mode
-  x |= SSTATUS_SPIE; // enable interrupts in user mode
-  w_sstatus(x);
+    // set up the registers that trampoline.S's sret will use
+    // to get to user space.
 
-  // set S Exception Program Counter to the saved user pc.
-  w_sepc(p->trapframe->epc);
+    // set S Previous Privilege mode to User.
+    unsigned long x = r_sstatus();
+    x &= ~SSTATUS_SPP; // clear SPP to 0 for user mode
+    x |= SSTATUS_SPIE; // enable interrupts in user mode
+    w_sstatus(x);
 
-  // tell trampoline.S the user page table to switch to.
-  uint64 satp = MAKE_SATP(p->pagetable);
+    // set S Exception Program Counter to the saved user pc.
+    w_sepc(p->trapframe->epc);
 
-  // jump to trampoline.S at the top of memory, which 
-  // switches to the user page table, restores user registers,
-  // and switches to user mode with sret.
-  uint64 fn = TRAMPOLINE + (userret - trampoline);
-  ((void (*)(uint64,uint64))fn)(TRAPFRAME, satp);
+    // tell trampoline.S the user page table to switch to.
+    uint64 satp = MAKE_SATP(p->pagetable);
+
+    // jump to trampoline.S at the top of memory, which
+    // switches to the user page table, restores user registers,
+    // and switches to user mode with sret.
+    uint64 fn = TRAMPOLINE + (userret - trampoline);
+    ((void (*)(uint64, uint64)) fn)(TRAPFRAME, satp);
 }
 
 // interrupts and exceptions from kernel code go here via kernelvec,
 // on whatever the current kernel stack is.
-void 
-kerneltrap()
-{
-  int which_dev = 0;
-  uint64 sepc = r_sepc();
-  uint64 sstatus = r_sstatus();
-  uint64 scause = r_scause();
-  
-  if((sstatus & SSTATUS_SPP) == 0)
-    panic("kerneltrap: not from supervisor mode");
-  if(intr_get() != 0)
-    panic("kerneltrap: interrupts enabled");
+void
+kerneltrap() {
+    int which_dev = 0;
+    uint64 sepc = r_sepc();
+    uint64 sstatus = r_sstatus();
+    uint64 scause = r_scause();
 
-  if((which_dev = devintr()) == 0){
-    printf("scause %p\n", scause);
-    printf("sepc=%p stval=%p\n", r_sepc(), r_stval());
-    panic("kerneltrap");
-  }
+    if ((sstatus & SSTATUS_SPP) == 0)
+        panic("kerneltrap: not from supervisor mode");
+    if (intr_get() != 0)
+        panic("kerneltrap: interrupts enabled");
 
-  // give up the CPU if this is a timer interrupt.
-  if(which_dev == 2 && myproc() != 0 && myproc()->state == RUNNING)
-    yield();
+    if ((which_dev = devintr()) == 0) {
+        printf("scause %p\n", scause);
+        printf("sepc=%p stval=%p\n", r_sepc(), r_stval());
+        panic("kerneltrap");
+    }
 
-  // the yield() may have caused some traps to occur,
-  // so restore trap registers for use by kernelvec.S's sepc instruction.
-  w_sepc(sepc);
-  w_sstatus(sstatus);
+    // give up the CPU if this is a timer interrupt.
+    if (which_dev == 2 && myproc() != 0 && myproc()->state == RUNNING)
+        yield();
+
+    // the yield() may have caused some traps to occur,
+    // so restore trap registers for use by kernelvec.S's sepc instruction.
+    w_sepc(sepc);
+    w_sstatus(sstatus);
 }
 
 void
-clockintr()
-{
-  acquire(&tickslock);
-  ticks++;
-  wakeup(&ticks);
-  release(&tickslock);
+clockintr() {
+    acquire(&tickslock);
+    ticks++;
+    wakeup(&ticks);
+    release(&tickslock);
 }
 
 // check if it's an external interrupt or software interrupt,
@@ -199,47 +193,46 @@ clockintr()
 // 1 if other device,
 // 0 if not recognized.
 int
-devintr()
-{
-  uint64 scause = r_scause();
+devintr() {
+    uint64 scause = r_scause();
 
-  if((scause & 0x8000000000000000L) &&
-     (scause & 0xff) == 9){
-    // this is a supervisor external interrupt, via PLIC.
+    if ((scause & 0x8000000000000000L) &&
+        (scause & 0xff) == 9) {
+        // this is a supervisor external interrupt, via PLIC.
 
-    // irq indicates which device interrupted.
-    int irq = plic_claim();
+        // irq indicates which device interrupted.
+        int irq = plic_claim();
 
-    if(irq == UART0_IRQ){
-      uartintr();
-    } else if(irq == VIRTIO0_IRQ){
-      virtio_disk_intr();
-    } else if(irq){
-      printf("unexpected interrupt irq=%d\n", irq);
+        if (irq == UART0_IRQ) {
+            uartintr();
+        } else if (irq == VIRTIO0_IRQ) {
+            virtio_disk_intr();
+        } else if (irq) {
+            printf("unexpected interrupt irq=%d\n", irq);
+        }
+
+        // the PLIC allows each device to raise at most one
+        // interrupt at a time; tell the PLIC the device is
+        // now allowed to interrupt again.
+        if (irq)
+            plic_complete(irq);
+
+        return 1;
+    } else if (scause == 0x8000000000000001L) {
+        // software interrupt from a machine-mode timer interrupt,
+        // forwarded by timervec in kernelvec.S.
+
+        if (cpuid() == 0) {
+            clockintr();
+        }
+
+        // acknowledge the software interrupt by clearing
+        // the SSIP bit in sip.
+        w_sip(r_sip() & ~2);
+
+        return 2;
+    } else {
+        return 0;
     }
-
-    // the PLIC allows each device to raise at most one
-    // interrupt at a time; tell the PLIC the device is
-    // now allowed to interrupt again.
-    if(irq)
-      plic_complete(irq);
-
-    return 1;
-  } else if(scause == 0x8000000000000001L){
-    // software interrupt from a machine-mode timer interrupt,
-    // forwarded by timervec in kernelvec.S.
-
-    if(cpuid() == 0){
-      clockintr();
-    }
-    
-    // acknowledge the software interrupt by clearing
-    // the SSIP bit in sip.
-    w_sip(r_sip() & ~2);
-
-    return 2;
-  } else {
-    return 0;
-  }
 }
 
